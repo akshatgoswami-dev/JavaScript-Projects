13e70db5577c6df257c3fbc7a6c9d145b8157e10
akshatgoswami-dev <akshatgoswami036@gmail.com>
2026-07-06 18:53:48 +0530

    Add HTML structure and styling for Digital Calculator; implement button functionality in JavaScript

--- Project-1, Digital Calculator/script.js
+++ Project-1, Digital Calculator/script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+let buttons = document.querySelectorAll(".btn-style");
+let display = document.querySelector("#display");
+
+buttons.forEach((val) => {
+  val.addEventListener("click", (e) => {
+    // console.dir(val);
+    let content = e.target.textContent;
+    console.dir(content);
+    display.value += content;
+
+    
+  });
+});
